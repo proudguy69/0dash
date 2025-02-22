068c57150338b2e43f171e162fa52a6ddd51fbe7
ProudGuy69 <idioticbatteringram@gmail.com>
2025-02-21 22:12:36 -0500

    added frontend support for backend

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <template>
-  
+  <router-view></router-view>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
+
+
+
+//get authorization data
+const session_data = ref({})
+
+
+async function getSession() {
+    const response = await fetch('/api/get/session')
+    const response_json = await response.json()
+    return response_json
+}
+
+// on first load
+onMounted(async ()=> {
+    session_data.value = getSession()
+})
+
 
 </script>
 
