e8af43ab5e47cda1a1412568a19cbc490eeda70e
ProudGuy69 <idioticbatteringram@gmail.com>
2025-02-21 22:12:16 -0500

    added backend and basic auth data

---
+++ backend/main.py
@@ -0,0 +1,40 @@
+import requests
+from settings import CLIENT_SECRET
+from flask import Flask, request, session, jsonify, redirect
+
+
+API_ENDPOINT = 'https://discord.com/api/v10'
+CLIENT_ID = '1342691151693615225'
+REDIRECT_URI = 'localhost:5174/'
+
+def exchange_code(code):
+  data = {
+    'grant_type': 'authorization_code',
+    'code': code,
+    'redirect_uri': REDIRECT_URI
+  }
+  headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+  r = requests.post(f'{API_ENDPOINT}/oauth2/token', data=data, headers=headers, auth=(CLIENT_ID, CLIENT_SECRET))
+  return r.json()
+
+
+app = Flask(__name__)
+app.secret_key = 'afjhiuashdf'
+
+
+@app.route('/api/authorize')
+def authorize():
+  code = request.args.get('code')
+  exchanged_code = exchange_code(code)
+  session['access'] = exchanged_code
+  return redirect('/') # return the user upon successful authorization
+
+@app.route('/api/get/session')
+def get_session():
+  session_data = session.get('access', None)
+  return jsonify({'sucess':True if session_data else False,'session_data':session_data})
+
+
+app.run(debug=True)
